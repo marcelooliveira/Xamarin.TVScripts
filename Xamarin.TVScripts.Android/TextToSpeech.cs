fb4e3ad81da8b307a76455d3bfded562bae9893c
marcelooliveira <mclricardo@gmail.com>
2018-03-30 12:27:39 -0300

    .

--- Xamarin.TVScripts.Android/TextToSpeech.cs
+++ Xamarin.TVScripts.Android/TextToSpeech.cs
@@ -14,12 +14,12 @@ using Android.Widget;
 using Xamarin.TVScripts.Droid;
 using Xamarin.TVScripts.Services;
 
-[assembly: Xamarin.Forms.Dependency(typeof(TextToSpeechImplementation))]
+[assembly: Xamarin.Forms.DependencyAttribute(typeof(Xamarin.TVScripts.Droid.TextToSpeech))]
 namespace Xamarin.TVScripts.Droid
 {
-    public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    public class TextToSpeech : Java.Lang.Object, ITextToSpeech, global::Android.Speech.Tts.TextToSpeech.IOnInitListener
     {
-        TextToSpeech speaker;
+        global::Android.Speech.Tts.TextToSpeech speaker;
         string toSpeak;
 
         public void Speak(string text)
@@ -27,7 +27,7 @@ namespace Xamarin.TVScripts.Droid
             toSpeak = text;
             if (speaker == null)
             {
-                speaker = new TextToSpeech(Application.Context, this);
+                speaker = new global::Android.Speech.Tts.TextToSpeech(Application.Context, this);
             }
             else
             {
